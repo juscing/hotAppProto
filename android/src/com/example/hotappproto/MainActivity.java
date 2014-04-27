025528ab67a3e43eb4c61b586cd723af210d4d6c
juscing <juscing@gmail.com>
2014-04-27 16:19:24 -0400

    Add icons and BG images. Fix the overlay better

--- android/src/com/example/hotappproto/MainActivity.java
+++ android/src/com/example/hotappproto/MainActivity.java
@@ -4,15 +4,14 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.touchmenotapps.widget.radialmenu.menu.v2.RadialMenuItem;
@@ -20,16 +19,7 @@ import com.touchmenotapps.widget.radialmenu.menu.v2.RadialMenuRenderer;
 import com.touchmenotapps.widget.radialmenu.menu.v2.RadialMenuRenderer.OnRadailMenuClick;
 
 public class MainActivity extends Activity {
-
-	private static final int[] ITEM_DRAWABLES = { R.drawable.ic_launcher, R.drawable.ic_launcher,
-		R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher };
 	
-	private ArrayList<RadialMenuItem> items;
-	
-	private RadialMenuRenderer mRenderer;
-	RadialMenuItem menuItem;
-	RadialMenuItem menuItem2;
-	RadialMenuItem menuItem3;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,50 +31,11 @@ public class MainActivity extends Activity {
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 		
-		FrameLayout v = (FrameLayout) findViewById(R.id.container);
-		
-		items = new ArrayList<RadialMenuItem>();
-		
-		mRenderer = new RadialMenuRenderer(v, true, 60, 120);
-		menuItem = new RadialMenuItem("Test","Test");
-		menuItem2 = new RadialMenuItem("Test","Test");
-		menuItem3 = new RadialMenuItem("Test","Test");
 		
 		
 		
-		menuItem.setOnRadialMenuClickListener(new OnRadailMenuClick() {
-			@Override
-			public void onRadailMenuClickedListener(String id) {
-				//Can edit based on preference. Also can add animations here.
-				Toast.makeText(MainActivity.this, "One", Toast.LENGTH_SHORT).show();
-			}
-		});
-		
-		menuItem2.setOnRadialMenuClickListener(new OnRadailMenuClick() {
-			@Override
-			public void onRadailMenuClickedListener(String id) {
-				//Can edit based on preference. Also can add animations here.
-				Toast.makeText(MainActivity.this, "Two", Toast.LENGTH_SHORT).show();
-			}
-		});
-		
-		menuItem3.setOnRadialMenuClickListener(new OnRadailMenuClick() {
-			@Override
-			public void onRadailMenuClickedListener(String id) {
-				//Can edit based on preference. Also can add animations here.
-				Toast.makeText(MainActivity.this, "Three", Toast.LENGTH_SHORT).show();
-			}
-		});
-		
-		
-		
-		
-		items.add(menuItem);
-		items.add(menuItem2);
-		items.add(menuItem3);
-		mRenderer.setRadialMenuContent(items);
 	    
-	    v.addView(mRenderer.renderView());
+	    
 	    /*
 	    v.setOnTouchListener(new OnTouchListener() {
 
@@ -124,7 +75,14 @@ public class MainActivity extends Activity {
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
-
+		
+		private ArrayList<RadialMenuItem> items;
+		
+		RadialMenuRenderer mRenderer;
+		RadialMenuItem menuItem;
+		RadialMenuItem menuItem2;
+		RadialMenuItem menuItem3;
+		
 		public PlaceholderFragment() {
 		}
 
@@ -134,8 +92,50 @@ public class MainActivity extends Activity {
 			View rootView = inflater.inflate(R.layout.fragment_main, container,
 					false);
 			
+			RelativeLayout v = (RelativeLayout) rootView.findViewById(R.id.rellayout1);
+			mRenderer = new RadialMenuRenderer(v, true, 60, 120);
+			mRenderer.setMenuBackgroundColor(0xcc000000);
+			mRenderer.setMenuSelectedColor(0xdd0069C4);
+			menuItem = new RadialMenuItem("Test","Test");
+			menuItem2 = new RadialMenuItem("Test","Test");
+			menuItem3 = new RadialMenuItem("Test","Test");
 			
-		    
+			items = new ArrayList<RadialMenuItem>();
+			
+			menuItem.setOnRadialMenuClickListener(new OnRadailMenuClick() {
+				@Override
+				public void onRadailMenuClickedListener(String id) {
+					//Can edit based on preference. Also can add animations here.
+					Toast.makeText(getActivity(), "One", Toast.LENGTH_SHORT).show();
+				}
+			});
+			
+			menuItem2.setOnRadialMenuClickListener(new OnRadailMenuClick() {
+				@Override
+				public void onRadailMenuClickedListener(String id) {
+					//Can edit based on preference. Also can add animations here.
+					Toast.makeText(getActivity(), "Two", Toast.LENGTH_SHORT).show();
+				}
+			});
+			
+			menuItem3.setOnRadialMenuClickListener(new OnRadailMenuClick() {
+				@Override
+				public void onRadailMenuClickedListener(String id) {
+					//Can edit based on preference. Also can add animations here.
+					Toast.makeText(getActivity(), "Three", Toast.LENGTH_SHORT).show();
+				}
+			});
+			
+			
+			
+			
+			items.add(menuItem);
+			items.add(menuItem2);
+			items.add(menuItem3);
+			mRenderer.setRadialMenuContent(items);
+			
+			v.addView(mRenderer.renderView());
+			
 			return rootView;
 		}
 		
